f6adda34da58aea75b8561e3933dcbe1933745d2
vierlan <28763492+vierlan@users.noreply.github.com>
2025-07-01 12:07:57 +0100

    CRDL:344:Add test-only endpoints which clear the excise-products, cn-codes and codelists collections

--- app/uk/gov/hmrc/emcstfereferencedata/controllers/testonly/TestOnlyController.scala
+++ app/uk/gov/hmrc/emcstfereferencedata/controllers/testonly/TestOnlyController.scala
@@ -19,7 +19,7 @@ package uk.gov.hmrc.emcstfereferencedata.controllers.testonly
 import org.mongodb.scala.*
 import org.mongodb.scala.model.Filters
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import uk.gov.hmrc.emcstfereferencedata.repositories.CnCodesRepository
+import uk.gov.hmrc.emcstfereferencedata.repositories.{CnCodesRepository, ExciseProductsRepository, CodeListsRepository}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
@@ -28,12 +28,28 @@ import scala.concurrent.ExecutionContext
   @Singleton
   class TestOnlyController @Inject() (
     cc: ControllerComponents,
-    codeListsRepository: CnCodesRepository
+    codeListsRepository: CodeListsRepository,
+    exciseProductsRepository: ExciseProductsRepository,
+    cnCodesRepository: CnCodesRepository
   )(using ec: ExecutionContext)
     extends BackendController(cc) {
 
     def deleteCodeLists(): Action[AnyContent] = Action.async {
       codeListsRepository.collection.deleteMany(Filters.empty()).toFuture().map {
+        case result if result.wasAcknowledged() => Ok
+        case _                                  => InternalServerError
+      }
+    }
+    
+    def deleteExciseProducts(): Action[AnyContent] = Action.async {
+      exciseProductsRepository.collection.deleteMany(Filters.empty()).toFuture().map {
+        case result if result.wasAcknowledged() => Ok
+        case _                                  => InternalServerError
+      }
+    }
+    
+    def deleteCnCodes(): Action[AnyContent] = Action.async {
+      cnCodesRepository.collection.deleteMany(Filters.empty()).toFuture().map {
         case result if result.wasAcknowledged() => Ok
         case _                                  => InternalServerError
       }
