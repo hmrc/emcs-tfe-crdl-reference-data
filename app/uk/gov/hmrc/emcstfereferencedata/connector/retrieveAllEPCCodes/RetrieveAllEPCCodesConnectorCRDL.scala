cd81e37e694b4e4a3159be8ca344e98bd4a9547d
vierlan <28763492+vierlan@users.noreply.github.com>
2025-07-10 11:50:02 +0100

    added unit tests

--- app/uk/gov/hmrc/emcstfereferencedata/connector/retrieveAllEPCCodes/RetrieveAllEPCCodesConnectorCRDL.scala
+++ app/uk/gov/hmrc/emcstfereferencedata/connector/retrieveAllEPCCodes/RetrieveAllEPCCodesConnectorCRDL.scala
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.emcstfereferencedata.connector.retrieveAllEPCCodes
 
+import play.api.Logger
 import uk.gov.hmrc.emcstfereferencedata.models.response.{ErrorResponse, ExciseProductCode}
 import uk.gov.hmrc.emcstfereferencedata.repositories.ExciseProductsRepository
 import uk.gov.hmrc.http.HeaderCarrier
@@ -11,21 +28,23 @@ class RetrieveAllEPCCodesConnectorCRDL @Inject() (
   repository: ExciseProductsRepository
 ) extends RetrieveAllEPCCodesConnector {
 
+  lazy val logger: Logger = Logger(this.getClass)
+
   override def retrieveAllEPCCodes()(implicit
     ec: ExecutionContext,
     hc: HeaderCarrier
   ): Future[Either[ErrorResponse, Seq[ExciseProductCode]]] = {
     repository
       .fetchAllEPCCodes()
-      .map(Right())
-      .recover({
-        case ex: Exception => {
+      .map(Right(_))
+      .recover {
+        case exception: Exception => {
           logger.warn(
-            "[RetrieveAllEPCCodesConnectorCRDL][retrieveAllEPCCodes] Unexpected Error fetching data from repository",
-            ex
+            s"[RetrieveCnCodeInformationConnectorCRDL][retrieveCnCodeInformation] Unexpected Error fetching data from repository"
           )
+          Left(ErrorResponse.UnexpectedDownstreamResponseError)
         }
-      })
+      }
 
   }
 }
---
+++ test/uk/gov/hmrc/emcstfereferencedata/connector/retrieveAllEPCCodes/RetrieveAllEPCCodesConnectorCRDLSpec.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.emcstfereferencedata.connector.retrieveAllEPCCodes
+
+import org.mockito.ArgumentMatchers.any
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import uk.gov.hmrc.emcstfereferencedata.fixtures.BaseFixtures
+import uk.gov.hmrc.emcstfereferencedata.models.response.{ErrorResponse, ExciseProductCode}
+import uk.gov.hmrc.emcstfereferencedata.repositories.ExciseProductsRepository
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.Future
+
+class RetrieveAllEPCCodesConnectorCRDLSpec
+  extends AsyncWordSpec
+  with Matchers
+  with MockitoSugar
+  with BaseFixtures {
+
+  private val repository = mock[ExciseProductsRepository]
+  private val connector  = new RetrieveAllEPCCodesConnectorCRDL(repository)
+
+  given HeaderCarrier = HeaderCarrier()
+
+  val exciseItemsList = Seq(
+    ExciseProductCode(
+      code = "W200",
+      description = "Still wine and still fermented beverages other than wine and beer",
+      category = "W",
+      categoryDescription = "Wine and fermented beverages other than wine and beer"
+    ),
+    ExciseProductCode(
+      code = "B000",
+      description = "Beer",
+      category = "B",
+      categoryDescription = "Beer"
+    ),
+    ExciseProductCode(
+      code = "E200",
+      description =
+        "Vegetable and animal oils Products falling within CN codes 1507 to 1518, if these are intended for use as heating fuel or motor fuel (Article 20(1)(a))",
+      category = "E",
+      categoryDescription = "Energy Products"
+    ),
+    ExciseProductCode(
+      code = "E300",
+      description =
+        "Mineral oils Products falling within CN codes 2707 10, 2707 20, 2707 30 and 2707 50 (Article 20(1)(b))",
+      category = "W" +
+        "E",
+      categoryDescription = "Energy Products"
+    )
+  )
+
+  "RetrieveAllEPCCodesConnectorCRDL.retrieveAllEPCCodes" should {
+    "Return a Map of Cncode to CnCodeInformation " in {
+      when(repository.fetchAllEPCCodes())
+        .thenReturn(
+          Future.successful(exciseItemsList)
+        )
+
+      connector.retrieveAllEPCCodes()
+        .map(
+          _ shouldBe Right(exciseItemsList)
+        )
+    }
+
+    "Return an empty list" when {
+      "there are no Excise items in the database" in {
+        when(repository.fetchAllEPCCodes())
+          .thenReturn(Future.successful(Seq.empty))
+
+        connector.retrieveAllEPCCodes()
+          .map(_ shouldBe Right(Seq.empty))
+
+      }
+    }
+    "return an Error Response" when {
+      "there is a error fetching data" in {
+        when(repository.fetchAllEPCCodes())
+          .thenReturn(Future.failed(new RuntimeException("Simulated failure")))
+
+        connector.retrieveAllEPCCodes()
+          .map(_ shouldBe Left(ErrorResponse.UnexpectedDownstreamResponseError))
+      }
+    }
+  }
+
+}
